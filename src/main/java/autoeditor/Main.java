ad5dd335d6c379fd15eadd5c124033cc29eaa1df
t4w51f <tawsifhasan1998@yahoo.com>
2020-05-16 14:39:28 -0700

    - Reset local main

--- src/main/java/autoeditor/Main.java
+++ src/main/java/autoeditor/Main.java
@@ -6,19 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main {//extends Application {
-//
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("AutoEditorUI.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
-//    }
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("AutoEditorUI.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
 
 
     public static void main(String[] args) {
-        //launch(args);
-        VideoIntelligenceResponseParser.getTimeRanges();
+        launch(args);
     }
 }
