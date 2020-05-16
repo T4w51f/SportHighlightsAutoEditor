b3482b0311e0402539fb92b8a39c4980d789bb21
Ahnaf <ahnaf452000@gmail.com>
2020-05-16 14:45:15 -0700

    integrating fxml

--- src/main/java/autoeditor/Main.java
+++ src/main/java/autoeditor/Main.java
@@ -5,17 +5,49 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef;
+import static com.sun.jna.platform.win32.WinUser.GWL_STYLE;
 
 public class Main extends Application {
 
+    private double xOfffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         Parent root = FXMLLoader.load(getClass().getResource("AutoEditorUI.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+        primaryStage.setTitle("Video Highlights Generator");
+        Scene scene = new Scene(root, 300, 275);
+        scene.getStylesheets().addAll(this.getClass().getResource("stylesheet.css").toExternalForm());
 
+        //grab root and move window
+        root.setOnMousePressed(event -> {
+            xOfffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        root.setOnMouseDragEntered(event -> {
+            primaryStage.setX(event.getScreenX() - xOfffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        minimizeIconTray();
+    }
+    private static void minimizeIconTray(){
+        long lhwnd = com.sun.glass.ui.Window.getWindows().get(0).getNativeWindow();
+        Pointer lpVoid = new Pointer(lhwnd);
+        WinDef.HWND hwnd = new WinDef.HWND(lpVoid);
+        final User32 user32 = User32.INSTANCE;
+        int oldStyle = user32.GetWindowLong(hwnd, GWL_STYLE);
+        int newStyle = oldStyle | 0x00020000;//WS_MINIMIZEBOX
+        user32.SetWindowLong(hwnd, GWL_STYLE, newStyle);
+    }
 
     public static void main(String[] args) {
         launch(args);
